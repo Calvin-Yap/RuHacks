767351383b7f02b9f2b2356ac103562cb2f77af5
Calvin Yap <calvin.yap@ryerson.ca>
2019-05-18 02:55:30 -0400

    react router config

---
+++ tracktor/src/components/charity/charity.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Charity extends React.Component{
+  render(){
+    document.body.style.backgroundColor= "white";
+    //document.body.style.backgroundSize = "cover";
+    //document.body.style.backgroundAttachment = "fixed";
+        return (
+      <div>
+      <p> hi</p>
+    </div>
+    )
+  }
+}
+
+export default Charity;
