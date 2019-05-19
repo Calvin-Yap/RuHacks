172dd090ac53d9e0f077e1a21c2dd5211df2d3c2
Samuel Chow <40840173+samuel-cee@users.noreply.github.com>
2019-05-19 04:58:19 -0400

    hi

--- tracktor/src/components/charity/charity.js
+++ tracktor/src/components/charity/charity.js
@@ -1,66 +1,78 @@
 import React from 'react';
-import Navigation from "../home/navbar";
 import './charity.css';
 
 
 
 class Charity extends React.Component {
   render() {
-    document.body.style.backgroundColor = "white";
-    document.body.style.background = "url('http://prod-upp-image-read.ft.com/52afd056-01ab-11e2-8aaa-00144feabdc0') no-repeat center 200px ";
+    document.body.style.background = "url('https://brockportchamber.org/wp-content/uploads/2017/09/happy-family.jpg') no-repeat center center ";
     document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
     document.body.style.backgroundSize = "100% 100%";
-
     return (
       <div>
-        <div className="navbar">
-          <img className="logo" alt="logo" src="https://i.imgur.com/xOvpeIV.png" />
-          <Navigation />
+        <div className="cnavigationalbar">
+          <img className="clogo2" alt="logo" src="https://i.imgur.com/iwrkQ7t.png" />
+          <p className="ctext"><span className="underline1">Home</span></p>
+          <p className="ctext1"><span className="underline1">About</span></p>
+          <p className="ctext2"><span className="underline1">Media</span></p>
+          <p className="ctext3"><span className="underline1">Contact</span></p>
+          <p className="ctext4"><span className="underline1">Log On</span></p>
         </div>
-        <div class="box">
-          <h1 id="bold" className="p1">The Salvation Army</h1>
-          <br></br><br></br>
-          <img className="picture1" alt="logo" src="https://www.wmbfnews.com/resizer/WfXEhR-PwPbHo2LhlHU2YH5m2h8=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/FJ7F4UZNKBCATOKPHF55MDQIEA.jpg" />
-          <br></br>
-          <br></br>
-          <p className="p1">The Salvation Army is a Christian organization that gives hope
-                      and support to vulnerable people in 400 communities across Canada and in 131 countries.
-                      The Salvation Army exists to share the love of Jesus Christ, meet human needs and be a transforming influence in the communities of our world.
-          </p>
-          <p className="p1">
-            <a href="https://salvationarmy.ca/" class="btn btn-info" role="button">Learn More</a>
-          </p>
-        </div >
 
-        <br></br>
-        <div class="box">
-          <h1 id="bold" className="p1">Ronald McDonald House Charities</h1>
-          <br></br><br></br>
-          <img className="picture1" alt="logo" src="https://i.imgur.com/F88yUcT.png" />
-          <br></br>
-          <br></br>
-          <p className="p1">The mission of RMHC is to create, find and support programs that directly improve the health and well-being of children and their families. A world where all children have access to medical care, and their families are supported and actively involved in their children's care.
-          </p>
-          <p className="p1">
-            <a href="https://www.rmhccanada.ca/" class="btn btn-info" role="button">Learn More</a>
+        <div className="crow">
+          <div className="ccolumn">
+            <div className="cbox">
+              <h1 id="bold" className="cp1">The Salvation Army</h1>
+              <br></br>
+              <br></br>
+              <img className="cpicture1" alt="logo" src="https://www.wmbfnews.com/resizer/WfXEhR-PwPbHo2LhlHU2YH5m2h8=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/FJ7F4UZNKBCATOKPHF55MDQIEA.jpg" />
+              <br></br>
+              <br></br>
+              <p className="cp1">The Salvation Army is a Christian organization that gives hope
+                          and support to vulnerable people in 400 communities across Canada and in 131 countries.
+                          The Salvation Army exists to share the love of Jesus Christ, meet human needs and be a transforming influence in the communities of our world.
+            </p>
+              <br></br>
+              <p className="cp1">
+                <a href="https://salvationarmy.ca/" class="btn btn-info" role="button">Learn More</a>
+              </p>
+            </div >
+          </div>
+          <div className="ccolumn">
+            <div class="cbox">
+              <h1 id="bold" className="cp1">Ronald McDonald House Charities</h1>
+              <br></br><br></br>
+              <img className="cpicture1" alt="logo" src="https://i.imgur.com/F88yUcT.png" />
+              <br></br>
+              <br></br>
+              <p className="cp1">The mission of RMHC is to create, find and support programs that directly improve the health and well-being of children and their families. A world where all children have access to medical care, and their families are supported and actively involved in their children's care.
+            </p>
+              <br></br>
+              <p className="cp1">
+                <a href="https://www.rmhccanada.ca/" class="btn btn-info" role="button">Learn More</a>
 
-          </p>
-        </div >
+              </p>
+            </div >
+          </div>
+          <div className="ccolumn">
+            <div class="cbox">
+              <h1 id="bold" className="cp1">SickKids foundation</h1>
+              <br></br><br></br>
+              <img className="cpicture1" alt="logo" src="https://www.searchenginepeople.com/wp-content/uploads/2013/04/SickKids-foundation-logo.jpg" />
+              <br></br>
+              <br></br>
+              <p className="cp1">The Hospital for Sick Children (SickKids) is a health-care community dedicated to improving the health of children. Our mission is to provide the best in family-centred, compassionate care, to lead in scientific and clinical advancement, and to prepare the next generation of leaders in child health.
+            </p>
+              <br></br>
 
-        <br></br>
-        <div class="box">
-          <h1 id="bold" className="p1">Ronald McDonald House Charities</h1>
-          <br></br><br></br>
-          <img className="picture1" alt="logo" src="https://www.searchenginepeople.com/wp-content/uploads/2013/04/SickKids-foundation-logo.jpg" />
-          <br></br>
-          <br></br>
-          <p className="p1">The Hospital for Sick Children (SickKids) is a health-care community dedicated to improving the health of children. Our mission is to provide the best in family-centred, compassionate care, to lead in scientific and clinical advancement, and to prepare the next generation of leaders in child health.
-          </p>
-          <p className="p1">
-            <a className="button1" href="https://www.sickkidsfoundation.com/" class="btn btn-info" role="button">Learn More</a>
+              <p className="cp1">
+                <a className="cbutton1" href="https://www.sickkidsfoundation.com/" class="btn btn-info" role="button">Learn More</a>
 
-          </p>
-        </div >
+              </p>
+            </div >
+          </div>
+        </div>
         <br></br>
       </div >
 
--- tracktor/src/components/home/home.js
+++ tracktor/src/components/home/home.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { NavLink }  from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './home.css';
 import Navigation from "./navbar";
-class Home extends React.Component{
-  render(){
-    document.body.style.background = "url('https://i.imgur.com/30KJRWO.png') no-repeat center center ";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundAttachment = "fixed";
-    document.body.style.backgroundSize="100% 100%";
+class Home extends React.Component {
+    render() {
+        document.body.style.background = "url('https://i.imgur.com/30KJRWO.png') no-repeat center center ";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundAttachment = "fixed";
+        document.body.style.backgroundSize = "100% 100%";
         return (
-      <div>
-          <img className="logo" alt="logo" src="https://i.imgur.com/xOvpeIV.png"/>
-<Navigation/>
-<div className="lshifter">    
-    <div className="row">
-            <div className="column">
-                <div className="boxes1"><NavLink  className="buttons" to="/local"><span className="underline">Buying Local</span></NavLink></div>
-            </div>
-            <div className="column">
-                <div className="boxes2"> <NavLink className="buttons" to="/charity"><span className="underline">Charities</span></NavLink></div>
-            </div>
-            <div className="column">
-                <div className="boxes2line"> <NavLink  className="buttons" to="/farmer"><span className="underline">Farmers Connect</span></NavLink></div>
+            <div className="navigationbar">
+                <img className="logo" alt="logo" src="https://i.imgur.com/xOvpeIV.png" />
+                <Navigation />
+                <div className="lshifter">
+                    <div className="row">
+                        <div className="column">
+                            <div className="boxes1"><NavLink className="buttons" to="/local"><span className="underline">Buying Local</span></NavLink></div>
+                        </div>
+                        <div className="column">
+                            <div className="boxes2"> <NavLink className="buttons" to="/charity"><span className="underline">Charities</span></NavLink></div>
+                        </div>
+                        <div className="column">
+                            <div className="boxes2line"> <NavLink className="buttons" to="/farmer"><span className="underline">Farmers Connect</span></NavLink></div>
+                        </div>
+                        <div className="column">
+                            <div className="boxes2line1"> <NavLink className="buttons" to="/global"><span className="underline">Global Operations</span></NavLink></div>
+                        </div>
+                    </div>
                 </div>
-            <div className="column">
-                <div className="boxes2line1"> <NavLink  className="buttons" to="/global"><span className="underline">Global Operations</span></NavLink></div>
             </div>
-        </div>
-    </div>
-</div>      
-    )
-  }
+        )
+    }
 }
 
 export default Home;
